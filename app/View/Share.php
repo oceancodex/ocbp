c7969cdf083f841d8157d6c873fdd78a5e7b4af0
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2024-08-01 22:29:40 +0700

    KDN-PC-02: Update

--- app/View/Share.php
+++ app/View/Share.php
@@ -8,23 +8,42 @@ use WPSPCORE\Base\BaseShare;
 
 class Share extends BaseShare {
 
+	public static ?self $instance = null;
+
+	/*
+	 *
+	 */
+
 	public function variables(): array {
 		$variables = [];
 
 		try {
-			$settings = SettingsModel::query()->where('key','settings')->first();
-			$settings = json_decode($settings['value'] ?? '', true);
+			$settings              = SettingsModel::query()->where('key', 'settings')->first();
+			$settings              = json_decode($settings['value'] ?? '', true);
 			$variables['settings'] = $settings;
+
+			// Maybe your custom share variables here...
 		}
-		catch (\Exception $e) {
-			Funcs::debug($e->getMessage());
-		} catch(\Throwable $e) {
-			Funcs::debug($e->getMessage());
+		catch (\Exception|\Throwable $e) {
+			Funcs::notice($e->getMessage() . ' <code>(' . __CLASS__ . ')</code>', 'error', true);
 		}
 
 		$variables['user'] = wp_get_current_user();
 
+		// Maybe your custom share variables here...
+
 		return $variables;
+	}
+
+	/*
+	 *
+	 */
+
+	public static function instance(): ?self {
+		if (!static::$instance) {
+			static::$instance = new self();
+		}
+		return static::$instance;
 	}
 
 }

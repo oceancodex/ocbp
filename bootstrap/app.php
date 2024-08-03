133d7ff8d240ceffe8e294d6d857ba096ae77ff1
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2024-08-03 22:48:59 +0700

    KDN-PC-02: Update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,4 +1,9 @@
 <?php
+if (PHP_VERSION_ID < 70400 || PHP_VERSION_ID >= 80000) {
+	wp_admin_notice('"WPSP" requires PHP version from 7.4.0 to below 8.0.0. Please check your PHP version!', ['type' => 'error', 'dismissible' => true]);
+	return;
+}
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use WPSP\routes\Api;
